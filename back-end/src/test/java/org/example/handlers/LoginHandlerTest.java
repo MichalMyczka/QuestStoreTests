4220022ce9dd805bf81a720ac359a2d827ce59c2
Michal Myczka <michalmorgoth@gmail.com>
2020-10-15 15:38:05 +0200

    handlers testing 2

--- back-end/src/test/java/org/example/handlers/LoginHandlerTest.java
+++ back-end/src/test/java/org/example/handlers/LoginHandlerTest.java
@@ -1,11 +1,14 @@
 package org.example.handlers;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import org.apache.commons.io.output.NullOutputStream;
 import org.example.DAO.LoginDAO;
 import org.example.model.Mentor;
 import org.example.model.User;
+import org.example.model.ValueObject.LoggedUser;
 import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,30 +17,52 @@ import org.mockito.Mockito;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpCookie;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
+import static jdk.internal.net.http.common.Log.headers;
 import static org.mockito.Mockito.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoginHandlerTest {
-
+    ObjectMapper mapper = new ObjectMapper();
+    Headers header;
+    HttpCookie cookie;
     @Test
     void handle() throws Exception {
         LoginDAO loginDAO = setupLoginDaoMock();
         HttpExchange httpExchange = setupHttpExchangeMock();
         LoginHandler loginHandler = new LoginHandler(null,loginDAO);
-
         loginHandler.handle(httpExchange);
+        User user = loginDAO.login("email@gmail.com","password");
+        LoggedUser loggedUser = new LoggedUser(
+                user.getUserDetailsID().toString(),
+                user.getEmail(),
+                user.getRole()
+        );
+
+        cookie = new HttpCookie("user", mapper.writeValueAsString(loggedUser));
+        List<String> testList = new ArrayList<>();
+        testList.add(cookie.toString());
 
         Assertions.assertAll(
-                () -> verify()
+                () -> verify(loginDAO,atLeast(1)).login("email@gmail.com","password"),
+                () -> verify(httpExchange,atLeast(1)).getResponseHeaders(),
+                () -> verify(httpExchange).getResponseBody(),
+                () -> assertEquals(header.get("Access-Control-Allow-Origin"), Collections.singletonList("*")),
+                () -> assertEquals(header.get("Set-Cookie"), testList),
+                () -> assertEquals(header.get("Content-type"), Collections.singletonList("application/json")),
+                () -> assertEquals(header.get("Access-Control-Allow-Origin"), Collections.singletonList("*")),
+                () -> assertEquals(header.get("Access-Control-Allow-Methods"), Collections.singletonList("*"))
         );
     }
 
     private HttpExchange setupHttpExchangeMock() {
-        Headers header = new Headers();
-
+        header = new Headers();
         String initialString = "email=email@gmail.com&password=password";
         InputStream targetStream = new ByteArrayInputStream(initialString.getBytes());
 
